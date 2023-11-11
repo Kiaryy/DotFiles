32d43f7e30f1dc82f7dc81628082386fb86e1654
Kiaryy <107123168+Kiaryy@users.noreply.github.com>
2023-11-10 22:53:36 -0300

    Create install.sh
    install script

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Check if paru is installed
+if ! command -v paru &> /dev/null; then
+    echo "paru is not installed. Installing paru first."
+    
+    # Install paru
+    git clone https://aur.archlinux.org/paru.git 
+    cd paru || exit 1
+    makepkg -si --noconfirm || exit 1
+    cd .. || exit 1
+    rm -rf paru || exit 1
+fi
+
+# Updates everything
+sudo pacman -Syu || exit 1
+
+# Installation of all the Pacman packages
+pacman_packages=(
+  dunst
+  waybar
+  thunar
+  tumbler
+  ffmpegthumbnailer
+  wofi
+  rofi
+  htop
+  pipewire-pulse
+  xdg-desktop-portal-hyprland
+)
+
+sudo pacman -S "${pacman_packages[@]}" || exit 1
+
+# Updates everything again
+sudo pacman -Syu || exit 1
+
+# Installation of aur packages using the previously installed paru aur helper
+aur_packages=(
+  polkit-dumb-agent-git
+  cava
+  tty-clock
+  archlinux-tweak-tool-git
+  hyprshot
+  hyprpicker-git
+  nwg-look
+)
+
+paru -S "${aur_packages[@]}" || exit 1
+
+# Guess what
+# We update again!!
+sudo pacman -Syu || exit 1
+# tbh i dont know if i have to update this much but i do it just to make sure everything will run smoothly
