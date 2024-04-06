ab1d0a6aaccb69ac403de59395053e7576f5b175
Kiaryy <martin360.moron@gmail.com>
2024-04-06 13:42:17 -0300

    theme changes

--- hypr/max_audio.sh
+++ hypr/max_audio.sh
@@ -5,7 +5,7 @@ MAX_VOLUME=100
 
 # Get the current volume
 current_volume=$(pactl list sinks | grep 'Volume: front-left' | awk '{print $5}' | sed 's/%//')
-
+echo "$current_volume"
 # Check if the current volume is greater than the maximum allowed
 if [ "$current_volume" -gt "$MAX_VOLUME" ]; then
     pactl set-sink-volume 0 "$MAX_VOLUME%"
