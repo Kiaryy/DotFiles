5492bddf8ed0a93414ee5a433e524ad585dda45f
Kiaryy <martin360.moron@gmail.com>
2025-02-18 11:06:02 -0300

    fixed 1px black border in screenshots

--- hypr/scripts/ScreenShot.sh
+++ hypr/scripts/ScreenShot.sh
@@ -1,5 +1,9 @@
 # Send notification after taking screenshot
 # Let the user edit it with swappy if they click on it
+coords=$(slurp)
+sleep 0.2
+grim -g "$coords" - | wl-copy
+
 ret_val=$(notify-send --action="opt1=Edit it with Swappy" "Screenshot Taken" "Copied to clipboard")
 
 case $ret_val in
