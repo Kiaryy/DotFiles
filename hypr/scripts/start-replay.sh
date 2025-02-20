d0df78414f763a9fc9f915e6fa1dd3e8fb974d72
Kiaryy <martin360.moron@gmail.com>
2025-02-20 10:38:01 -0300

    Clips have higher quality

--- hypr/scripts/start-replay.sh
+++ hypr/scripts/start-replay.sh
@@ -3,4 +3,4 @@
 pidof -q gpu-screen-recorder && exit 0
 video_path="$HOME/Videos"
 mkdir -p "$video_path"
-gpu-screen-recorder -w screen -f 60 -a default_output -r 120 -sc ~/.config/hypr/scripts/save-application-name.sh -c mp4 -o "$video_path" -ac aac
+gpu-screen-recorder -w screen -f 60 -a default_output -r 120 -q ultra -sc ~/.config/hypr/scripts/save-application-name.sh -c mp4 -o "$video_path" -ac aac
