f11465a69b75d8d8db1a5fc133378f9a2eb34b80
Kiaryy <martin360.moron@gmail.com>
2025-02-20 10:38:42 -0300

    Changed grim + slurp for grimblast, mainly because of the --freeze flag

--- hypr/scripts/ScreenShot.sh
+++ hypr/scripts/ScreenShot.sh
@@ -1,13 +1,21 @@
-# Send notification after taking screenshot
-# Let the user edit it with swappy if they click on it
-coords=$(slurp)
-sleep 0.2
-grim -g "$coords" - | wl-copy
+#!/bin/sh
 
+grimblast --freeze copy area 
+
+# ik its not great to rely on the clipboard for this but doing it with a variable involves storing the 
+# image somewhere and then feeding swappy its path. 
+# Besides its not like  you're going to copy something else right after taking a screenshot and before editing it
+
+# Also, known bug; you will still get prompted to edit the screenshot even if you cancel it with ESC
+# I couldn't figure how to avoid it
+# Its not that big of a deal anyways
+
+# Send notification after taking screenshot
 ret_val=$(notify-send --action="opt1=Edit it with Swappy" "Screenshot Taken" "Copied to clipboard")
 
 case $ret_val in
     "opt1")
+        # Let the user edit it with swappy if they click on it
         wl-paste | swappy -f -  
         ;;
 esac
