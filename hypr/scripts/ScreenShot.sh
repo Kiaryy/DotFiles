2d76665c531bb7d6b14ea388904638187ac1356f
Kiaryy <martin360.moron@gmail.com>
2025-02-21 10:06:46 -0300

    Fixed issue where notify-send would issue a notification when cancelling the screenshot

--- hypr/scripts/ScreenShot.sh
+++ hypr/scripts/ScreenShot.sh
@@ -2,13 +2,15 @@
 
 grimblast --freeze copy area 
 
+grimblast_ret=$? # We store the return value of grimblast
+
+if [[ $grimblast_ret == 1 ]]; then
+    exit 1 # Early exit because grimblast selection was cancelled 
+fi
+
 # ik its not great to rely on the clipboard for this but doing it with a variable involves storing the 
 # image somewhere and then feeding swappy its path. 
 # Besides its not like  you're going to copy something else right after taking a screenshot and before editing it
-
-# Also, known bug; you will still get prompted to edit the screenshot even if you cancel it with ESC
-# I couldn't figure how to avoid it
-# Its not that big of a deal anyways
 
 # Send notification after taking screenshot
 ret_val=$(notify-send --action="opt1=Edit it with Swappy" "Screenshot Taken" "Copied to clipboard")
