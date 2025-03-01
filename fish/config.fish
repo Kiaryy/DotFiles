338ee3a9f36034eef2f1ea4b3e0e815510e7a259
Kiaryy <martin360.moron@gmail.com>
2025-03-01 19:38:58 -0300

    Some changes that i forgot to add and now im adding at the same time (probably gonna introduce a million bugs but idc)

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,10 @@ if status is-interactive
     set -g tide_pwd_color_dirs AC3232
     set -g tide_pwd_color_dirs ffffff
     set -g set tide_git_icon  
+    set -g tide_status_bg_color_failure 000000  
+    set -g tide_status_bg_color 000000  
     neofetch --ascii ~/.config/neofetch/cat_ascii | lolcat --spread=20 --freq=0.01 -S 21520 -f
     alias tetrs="tetrs_terminal"
     alias pympeg="python /home/kiary/Downloads/scripts/pympeg.py"    
-
 end
+
